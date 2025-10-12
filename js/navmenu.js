afeab6fd95acdda77b612bbc88c9ad222f676e08
Rodion1102333 <50778474+Rodion1102333@users.noreply.github.com>
2025-10-12 16:11:17 +0300

    Delete js/navmenu.js
    update

--- js/navmenu.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.getElementById('nav-menu-container');  // Используем новый id для контейнера
-
-menuToggle.addEventListener('click', function() {
-  console.log('Меню переключается');
-  navMenu.classList.toggle('show');  // Переключаем класс show на контейнере меню
-  console.log('Текущее состояние:', navMenu.classList);  // Выводим текущее состояние
-});
